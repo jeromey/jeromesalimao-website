0833af44a8ff37017c3fa6644b9237a7f5b13ad4
Jerome Salimao <jerome.salimao@easyagile.com>
2022-06-21 18:10:08 +1000

    added images

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 (async function() {
-    console.log("here jerome");
     const cycleText = [
-        "a frontend developer 🤓",
-        "a plant guru 🪴",
-        "an adventurer 🏔",
-        "a former plant assassin 🪴🥷"
+        {text: "a frontend developer 🤓", image: "developer.jpg"},
+        {text: "a plant guru 🪴", image: "plant_guru.jpg"},
+        {text: "an adventurer 🏔", image: "adventurer.jpg"},
+        {text: "a former plant assassin 🪴🥷", image: "plant_assassin.jpg"}
     ];
 
     const cycleTextContainer = document.querySelector('#cycle-text-container');
+    const heroImage = document.querySelector("#hero-image");
+    const images = {};
 
     let i =0;
     setInterval(() => {
-        cycleTextContainer.innerHTML = cycleText[i];
+        cycleTextContainer.innerHTML = cycleText[i].text;
+        if (images[cycleText[i].image]) { heroImage.src =  images[cycleText[i].image].src; }
         i < cycleText.length -1? i++ : i=0;
-    }, 2000);
+    }, 2500);
 
+
+    cycleText.forEach(cycleObj => {
+        images[cycleObj.image] = new Image();
+        images[cycleObj.image].src = `images/${cycleObj.image}`;
+    })
 })();
